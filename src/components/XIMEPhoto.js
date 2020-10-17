48b754c26884db0fe075075ae4350ecaa6f3f8e1
oliverpdahl <oliverpdahl@gmail.com>
2020-10-17 10:36:56 -0600

    Photo and background working

--- src/components/XIMEPhoto.js
+++ src/components/XIMEPhoto.js
@@ -1,7 +1,20 @@
 import React from "react";
+import XIMEPhotoImage from "../graphics/XIME-Photo.png";
 
 function XIMEPhoto() {
-  return <div className="xime_photo"></div>;
+  return (
+    <div style={{ width: "100%" }}>
+      <img
+        style={{
+          height: "100vh",
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+        src={XIMEPhotoImage}
+      />
+    </div>
+  );
 }
 
 export default XIMEPhoto;
