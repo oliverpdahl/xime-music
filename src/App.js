3fcf5aebdd0dca5108f5420ddfa89ee0b9afc67d
oliverpdahl <oliverpdahl@gmail.com>
2020-10-17 09:52:33 -0600

    remove bad import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "./App.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 function App() {
   return (
     <div styles={{ backgroundImage: "./graphics/XIME-Background.png" }}></div>
